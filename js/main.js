6d598f6bb0e6f7d92d3d23a36757b2d399c493b2
Bryan Anderson <bryan@localhost.localdomain>
2015-07-18 16:27:55 -0500

    working on smooth scroll

--- js/main.js
+++ js/main.js
@@ -7,6 +7,11 @@ $(document).ready(function() {
     $('.navbar').css('background', 'rgba(153, 0, 0 ,0)');
   }
 
+  prevScrollTop = $(window).scrollTop();
+  var $root = $('html, body');
+  $root.animate({scrollTop: 0}, 0);
+  var down = 0;
+
   $(window).on('scroll', function() {
     if ($(window).scrollTop() > (topOfHeader + heightOfHeader - 68)) {
       $('.navbar').css('background', 'rgba(153, 0, 0 ,1)');
@@ -14,10 +19,28 @@ $(document).ready(function() {
       $('.navbar').css('background', 'rgba(153, 0, 0 ,0)');
     }
 
-  $('.btn').on('click', function() {
-    alert($(window).scrollTop());
-    alert(topOfHeader + heightOfHeader);
+    var newScrollTop = $(window).scrollTop();
+    if ($(window).scrollTop() < (topOfHeader + heightOfHeader - 68) && $(window).scrollTop() > 0 && newScrollTop > prevScrollTop) {
+      //scroll down to secondary header
+      //newScrollTop = topOfHeader + heightOfHeader - 67;
+      //alert('hi');
+      $root.animate({scrollTop: topOfHeader + heightOfHeader - 67}, 1000);
+    } else if ($(window).scrollTop() < (topOfHeader + heightOfHeader - 68) && $(window).scrollTop() > 0 && newScrollTop < prevScrollTop) {
+      //scroll up to welcome header
+      //newScrollTop = 0;
+      //alert('bye');
+      $root.animate({scrollTop: 0}, 1000);
+
+    } else if ($(window).scrollTop() > (topOfHeader + heightOfHeader - 66)) { 
+      //act like a normal website
+      //alert('ayyyy');
+      newScrollTop = $(window).scrollTop();
+      $root.css({scrollTop: $(window).scrollTop()});
+    }
+    prevScrollTop = newScrollTop;
   });
 
+  $('.btn').on('click', function() {
+    alert(newScrollTop);
   });
 });
